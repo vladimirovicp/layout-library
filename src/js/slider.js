620a1c050742982aa17520f4b3b64ed67b3e960a
vladimirovicp <vladimirovicp70@gmail.com>
2023-07-28 17:03:52 +0400

    feat: other

--- src/js/slider.js
+++ src/js/slider.js
@@ -1,104 +1,101 @@
-addEventListener("load", function(){
+const slider = document.querySelector('.slider');
+if(slider){
 
-    const slider = document.querySelector('.slider');
-    if(slider){
-        const arrowPrev = slider.querySelector('.slider__button-prev');
-        const arrowNext = slider.querySelector('.slider__button-next');
+    // const arrowPrev = slider.querySelector('.slider__button-prev');
+    // const arrowNext = slider.querySelector('.slider__button-next');
 
-        const arrows = slider.querySelectorAll('.slider__arrow');
+    const arrows = slider.querySelectorAll('.slider__arrow');
 
-        const wrapper = slider.querySelector('.slider__wrapper');
-        const firstImg = wrapper.querySelectorAll("img")[0];
+    const wrapper = slider.querySelector('.slider__wrapper');
+    const firstImg = wrapper.querySelectorAll("img")[0];
 
-
-
-        //console.log(firstImg)
-
-        arrows.forEach(arrow => {
-
-            arrow.addEventListener("click", () => {
-                //const indent = 2.5 * (Number((getComputedStyle(document.documentElement).fontSize).replace('px', '')) * 10) / 10;
-                let firstImgWidth = firstImg.clientWidth + 25;
-
-
-
-                //let firstImgWidth = Number((getComputedStyle(firstImg).width).replace('px', '')) + indent;
-
-
-                console.log(firstImg.clientWidth)
-                //console.log(Number((getComputedStyle(firstImg).width).replace('px', '')));
-
-                console.log(arrow)
-                console.log(arrow.classList.contains('slider__button-prev'))
-
-
-                //carousel.scrollLeft += icon.id == "left" ? -firstImgWidth : firstImgWidth;
-
-                wrapper.scrollLeft += arrow.classList.contains('slider__button-prev') ? -firstImgWidth : firstImgWidth;
-
-                //setTimeout(() => showHideIcons(), 60); // calling showHideIcons after 60ms
-            });
+    arrows.forEach(arrow => {
+        arrow.addEventListener("click", () => {
+            let firstImgWidth = firstImg.clientWidth + 25;
+            wrapper.scrollLeft += arrow.classList.contains('slider__button-prev') ? -firstImgWidth : firstImgWidth;
         });
+    });
+
+    const pagination = slider.querySelector('.slider__pagination');
+    const bullets = pagination.querySelectorAll('.slider__bullet');
+
+    bullets.forEach(bullet => {
+            bullet.addEventListener("click", () => {
+
+            });
+    });
 
 
 
 
-        // arrowPrev.addEventListener("click", () => {
-        //     //const indent = 2.5 * Number((getComputedStyle(document.documentElement).fontSize).replace('px', ''));
-        //     //let firstImgWidth = firstImg.clientWidth + indent;
-        //     const indent2 = 5 * (Number((getComputedStyle(document.documentElement).fontSize).replace('px', ''))* 100) / 100 ;
-        //     let firstImgWidth = (Number((getComputedStyle(wrapper).width).replace('px', '')) + indent2) / 3;
-        //     //let firstImgWidth = Number((getComputedStyle(wrapper).width).replace('px', '')) / 3;
-        //     wrapper.scrollLeft = wrapper.scrollLeft - firstImgWidth;
-        //
-        // });
-        //
-        // arrowNext.addEventListener("click", () => {
-        //
-        //     const fontSize = parseFloat(getComputedStyle(document.documentElement).fontSize);
-        //
-        //     console.log(fontSize)
-        //
-        //     //console.log('fontSize',fontSize)
-        //
-        //     //const indent = 2.5 * (Number((getComputedStyle(document.documentElement).fontSize).replace('px', '')));
-        //
-        //     const indent2 = 5 * (Number((getComputedStyle(document.documentElement).fontSize).replace('px', '')) * 10) / 10;
-        //
-        //     const indent = 2.5 * fontSize;
-        //
-        //     //const widthIMG = Math.round(Number((getComputedStyle(firstImg).width).replace('px', '')) * 100) / 100;
-        //
-        //     const widthIMG = 45 * fontSize;
-        //
-        //
-        //
-        //     //console.log('widthIMG',widthIMG)
-        //     //console.log(getComputedStyle(wrapper).width)
-        //
-        //
-        //     //console.log(widthIMG)
-        //     //let firstImgWidth = firstImg.clientWidth + indent;
-        //
-        //     //let firstImgWidth = widthIMG + indent;
-        //
-        //     let firstImgWidth = (Number((getComputedStyle(wrapper).width).replace('px', '')) + indent2) / 3;
-        //
-        //     //(firstImgWidth / 3)
-        //
-        //     //console.log(firstImgWidth)
-        //
-        //
-        //     //let firstImgWidth = getComputedStyle(firstImg).width + 25;
-        //     //console.log(firstImgWidth);
-        //    // console.log(getComputedStyle(wrapper).width)
-        //    // console.log(getComputedStyle(document.documentElement).fontSize)
-        //    // console.log(parseInt(getComputedStyle(document.documentElement).fontSize))
-        //     //console.log(Number((getComputedStyle(document.documentElement).fontSize).replace('px', '')));
-        //
-        //     wrapper.scrollLeft = wrapper.scrollLeft + firstImgWidth;
-        // });
 
 
-    }
-});
+    // arrowPrev.addEventListener("click", () => {
+    //     //const indent = 2.5 * Number((getComputedStyle(document.documentElement).fontSize).replace('px', ''));
+    //     //let firstImgWidth = firstImg.clientWidth + indent;
+    //     const indent2 = 5 * (Number((getComputedStyle(document.documentElement).fontSize).replace('px', ''))* 100) / 100 ;
+    //     let firstImgWidth = (Number((getComputedStyle(wrapper).width).replace('px', '')) + indent2) / 3;
+    //     //let firstImgWidth = Number((getComputedStyle(wrapper).width).replace('px', '')) / 3;
+    //     wrapper.scrollLeft = wrapper.scrollLeft - firstImgWidth;
+    //
+    // });
+    //
+    // arrowNext.addEventListener("click", () => {
+    //
+    //     const fontSize = parseFloat(getComputedStyle(document.documentElement).fontSize);
+    //
+    //     console.log(fontSize)
+    //
+    //     //console.log('fontSize',fontSize)
+    //
+    //     //const indent = 2.5 * (Number((getComputedStyle(document.documentElement).fontSize).replace('px', '')));
+    //
+    //     const indent2 = 5 * (Number((getComputedStyle(document.documentElement).fontSize).replace('px', '')) * 10) / 10;
+    //
+    //     const indent = 2.5 * fontSize;
+    //
+    //     //const widthIMG = Math.round(Number((getComputedStyle(firstImg).width).replace('px', '')) * 100) / 100;
+    //
+    //     const widthIMG = 45 * fontSize;
+    //
+    //
+    //
+    //     //console.log('widthIMG',widthIMG)
+    //     //console.log(getComputedStyle(wrapper).width)
+    //
+    //
+    //     //console.log(widthIMG)
+    //     //let firstImgWidth = firstImg.clientWidth + indent;
+    //
+    //     //let firstImgWidth = widthIMG + indent;
+    //
+    //     let firstImgWidth = (Number((getComputedStyle(wrapper).width).replace('px', '')) + indent2) / 3;
+    //
+    //     //(firstImgWidth / 3)
+    //
+    //     //console.log(firstImgWidth)
+    //
+    //
+    //     //let firstImgWidth = getComputedStyle(firstImg).width + 25;
+    //     //console.log(firstImgWidth);
+    //    // console.log(getComputedStyle(wrapper).width)
+    //    // console.log(getComputedStyle(document.documentElement).fontSize)
+    //    // console.log(parseInt(getComputedStyle(document.documentElement).fontSize))
+    //     //console.log(Number((getComputedStyle(document.documentElement).fontSize).replace('px', '')));
+    //
+    //     wrapper.scrollLeft = wrapper.scrollLeft + firstImgWidth;
+    // });
+
+    window.addEventListener('resize', function(event) {
+        wrapper.scrollLeft = 0;
+    }, true);
+
+}
+
+
+
+
+
+
+
+
