48f5554a9a1bbbb556656446973fe1d3198929d5
vladimirovicp <vladimirovicp70@gmail.com>
2023-08-16 17:39:50 +0400

    add: refactor mobile

--- src/js/burger.js
+++ src/js/burger.js
@@ -7,7 +7,35 @@ const menuWrapper = menu.querySelector('.menu__wrapper');
 
 if(iconMenu){
     iconMenu.addEventListener("click", function(e){
+        document.body.classList.toggle('_lock');
         menuWrapper.classList.toggle('open');
         iconMenu.classList.toggle('open');
+
+        document.onclick = function (e) {
+            if(e.target.className == "_lock"){
+                menuWrapper.classList.toggle('open');
+                iconMenu.classList.toggle('open');
+                document.body.classList.remove('_lock');
+            }
+        };
     });
 }
+
+const menuLinks = menuWrapper.querySelectorAll('.menu__item');
+
+if (menuLinks.length > 0){
+
+    menuLinks.forEach(menuLink => {
+        menuLink.addEventListener("click", onMenuLinkClick);
+    });
+    function onMenuLinkClick(e){
+        //menuWrapper.classList.remove('_active');
+        //menuWrapper.classList.remove('open');
+        menuWrapper.classList.toggle('open');
+        iconMenu.classList.toggle('open');
+        document.body.classList.remove('_lock');
+    }
+
+}
+
+
