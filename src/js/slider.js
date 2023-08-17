2e46febf12dbe9b7bd596507d275ace573517d3e
vladimirovicp <vladimirovicp@gmail.com>
2023-08-17 22:25:02 +0400

    main slider

--- src/js/slider.js
+++ src/js/slider.js
@@ -1,5 +1,5 @@
 const slider = document.querySelector('.slider');
-if(slider){
+if (slider) {
     const arrowPrev = slider.querySelector('.slider__button-prev');
     const arrowNext = slider.querySelector('.slider__button-next');
 
@@ -11,77 +11,83 @@ if(slider){
     let currentBullet = 1;
     const pagination = slider.querySelector('.slider__pagination');
     const bullets = pagination.querySelectorAll('.slider__bullet');
+    const imgWidth = firstImg.clientWidth + 25;
 
 
     arrows.forEach(arrow => {
         arrow.addEventListener("click", () => {
 
-            // if( (currentBullet < slides_max ) && (currentBullet > 0) ){
-            //
-            // }
+            arrowPrev.classList.add('disable')
+            arrowNext.classList.add('disable')
 
+            //let firstImgWidth = firstImg.clientWidth + 25;
+            let firstImgWidth = imgWidth;
+            wrapper.scrollLeft += arrow.classList.contains('slider__button-prev') ? -firstImgWidth : firstImgWidth;
+            let nextBullet = arrow.classList.contains('slider__button-prev') ? currentBullet - 1 : currentBullet + 1;
 
-            //disable
-
-
-                let firstImgWidth = firstImg.clientWidth + 25;
-                wrapper.scrollLeft += arrow.classList.contains('slider__button-prev') ? -firstImgWidth : firstImgWidth;
-
-                let nextBullet = arrow.classList.contains('slider__button-prev') ?  currentBullet - 1 :  currentBullet + 1;
-
-                bullets.forEach(bullet => {
-                    if(Number(bullet.dataset.bullet) === currentBullet ){
-                        bullet.classList.remove('active')
-                    }
-                    if(Number(bullet.dataset.bullet) === nextBullet ){
-                        bullet.classList.add('active');
-                    }
-                })
-                currentBullet = nextBullet;
-
-            arrowPrev.classList.remove('disable')
-            arrowNext.classList.remove('disable')
-
-                if(currentBullet === 1 ){
-                    arrowPrev.classList.remove('.disable')
-                    arrowNext.classList.add('disable')
+            bullets.forEach(bullet => {
+                if (Number(bullet.dataset.bullet) === currentBullet) {
+                    bullet.classList.remove('active')
                 }
-                if(currentBullet === 5 ){
-                    arrowPrev.classList.remove('disable')
-                    arrowNext.classList.add('disable')
+                if (Number(bullet.dataset.bullet) === nextBullet) {
+                    bullet.classList.add('active');
                 }
-
+            })
+            currentBullet = nextBullet;
+            disabledBullet();
 
         });
     });
 
 
     bullets.forEach(bullet => {
-            bullet.addEventListener("click", (event) => {
-                let nextBullet = bullet.dataset.bullet;
+        bullet.addEventListener("click", (event) => {
+            let nextBullet = Number(bullet.dataset.bullet);
 
-                bullets.forEach(item => {
-                    if(item.classList.contains('active')) {
-                        item.classList.remove('active')
-                    };
-                })
+            bullets.forEach(item => {
+                if (item.classList.contains('active')) {
+                    item.classList.remove('active')
+                };
+            })
 
-                if(currentBullet < nextBullet){
-                    let firstImgWidth = (firstImg.clientWidth + 25) * (nextBullet - 1);
-                    wrapper.scrollLeft += firstImgWidth;
+            if (currentBullet < nextBullet) {
+                let firstImgWidth = imgWidth * (nextBullet - currentBullet);
+                wrapper.scrollLeft += firstImgWidth;
+            } else {
+                let firstImgWidth = imgWidth * (currentBullet - nextBullet);
+                wrapper.scrollLeft -= firstImgWidth;
+            }
 
-                } else {
-                    let firstImgWidth = (firstImg.clientWidth + 25) * (currentBullet - nextBullet);
-                    wrapper.scrollLeft -= firstImgWidth;
-                }
+            currentBullet = nextBullet;
+            bullet.classList.add('active');
+            disabledBullet();
+        });
 
-                currentBullet = nextBullet;
-                bullet.classList.add('active');
-            });
+
     });
 
-    window.addEventListener('resize', function(event) {
+
+    function disabledBullet(){
+        arrowPrev.classList.remove('disable')
+        arrowNext.classList.remove('disable')
+
+        if (currentBullet === 1) {
+            arrowPrev.classList.add('disable')
+            arrowNext.classList.remove('.disable')
+        }
+        if (currentBullet === 5) {
+            console.log('disabledBullet = ',arrowPrev)
+            arrowPrev.classList.remove('disable')
+            arrowNext.classList.add('disable')
+        }
+
+    }
+
+
+
+    window.addEventListener('resize', function (event) {
         wrapper.scrollLeft = 0;
+        currentBullet = 1;
     }, true);
 
 }
